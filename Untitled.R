c42dab995f783b3fffaa224da257f69d946b2ee7
B210624 <107563694+B210624@users.noreply.github.com>
2023-06-08 18:58:08 +0900

    #YK

--- Untitled.R
+++ Untitled.R
@@ -1,6 +1,6 @@
 # This a file
 #
-#The aim is to filter ome cols and make a graph
+#The aim is to filter   some cols and make a graph
 install.packages("tidyverse")
 library(tidyverse)
 library(ggplot2)
